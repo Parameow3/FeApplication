04c110a2e0a31160db39fad45ef8b5de32e4d82f
Chhay <bunchay1280@gmail.com>
2023-06-15 21:25:48 +0700

    api staff

---
+++ app/src/main/java/com/example/myapplication/ui/adapter/CoursesAdapter.java
@@ -0,0 +1,82 @@
+package com.example.myapplication.ui.adapter;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.api.model.Course;
+import com.example.myapplication.databinding.ViewHolderCourseBinding;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class CoursesAdapter extends ListAdapter<Course, CoursesAdapter.CourseViewHolder> {
+
+    public CoursesAdapter() {
+        super(new DiffUtil.ItemCallback<Course>() {
+            @Override
+            public boolean areItemsTheSame(@NonNull Course oldItem, @NonNull Course newItem) {
+                return oldItem == newItem;
+            }
+
+            @Override
+            public boolean areContentsTheSame(@NonNull Course oldItem, @NonNull Course newItem) {
+                return oldItem.getId() == newItem.getId();
+            }
+        });
+
+
+    }
+
+
+    @NonNull
+    @Override
+    public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ViewHolderCourseBinding binding = ViewHolderCourseBinding.inflate(layoutInflater, parent, false);
+
+        return new CourseViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+
+        Course item = getItem(position);
+
+
+    }
+
+    public static class CourseViewHolder extends RecyclerView.ViewHolder {
+        ViewHolderCourseBinding itemBinding;
+        Bitmap bitmap;
+        public CourseViewHolder(ViewHolderCourseBinding itemBinding) {
+            super(itemBinding.getRoot());
+            this.itemBinding = itemBinding;
+        }
+
+        public void bind(Course course) throws IOException {
+            // bind image to recycle view
+            InputStream in = new URL(course.getImageUrl()).openStream();
+            bitmap = BitmapFactory.decodeStream(in);
+            itemBinding.imgCourse.setImageBitmap(bitmap);
+
+            // bind title to recycle view
+            itemBinding.txtCourseTitle.setText(course.getTitle());
+
+            Log.e("[title]", course.getTitle());
+
+            // bind description to recycle view
+            itemBinding.txtCourseDes.setText(course.getDescription());
+        }
+
+    }
+}
--- app/src/main/java/com/example/myapplication/ui/fragment/CoursesFragment.java
+++ app/src/main/java/com/example/myapplication/ui/fragment/CoursesFragment.java
@@ -1,16 +1,32 @@
 package com.example.myapplication.ui.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.myapplication.api.model.Course;
+import com.example.myapplication.api.service.ApiService;
 import com.example.myapplication.databinding.FragmentCoursesBinding;
 import com.example.myapplication.databinding.FragmentHomeBinding;
+import com.example.myapplication.ui.adapter.CoursesAdapter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CoursesFragment extends Fragment {
 
@@ -29,6 +45,53 @@ public class CoursesFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         // load list courses from server (api)
+        loadCourseListFromServer();
+    }
+
+    private void loadCourseListFromServer(){
+
+        // create retrofit client
+        Retrofit httpClient = new Retrofit.Builder()
+                .baseUrl("https://ferupp.s3.ap-southeast-1.amazonaws.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // create service object
+        ApiService apiService = httpClient.create(ApiService.class);
+
+        // load course list from server
+        Call<List<Course>> task = apiService.loadCourseList();
+        task.enqueue(new Callback<List<Course>>() {
+            @Override
+            public void onResponse(Call<List<Course>> call, Response<List<Course>> response) {
+                if (response.isSuccessful()){
+
+                    showCourseList(response.body());
+
+                }else {
+                    Toast.makeText(getContext(), "Load Courses list failed!", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Course>> call, Throwable t) {
+                Toast.makeText(getContext(), "Load Courses list failed!", Toast.LENGTH_LONG).show();
+                Log.e("[CoursesFragment]", "Load Courses failed: " + t.getMessage());
+            }
+        });
+    }
+
+    private void showCourseList(List<Course> courseList){
+
+        // Create Layout Manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        binding.recyclerViewCourse.setLayoutManager(layoutManager);
+
+        // Create Adapter
+        CoursesAdapter adapter = new CoursesAdapter();
+        adapter.submitList(courseList);
+        binding.recyclerViewCourse.setAdapter(adapter);
 
     }
 }
